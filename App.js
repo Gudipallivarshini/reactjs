8e2538bd0a3dec6bb0e1f5484b1549be40a3e786
Gudipallivarshini <78401857+Gudipallivarshini@users.noreply.github.com>
2021-02-06 16:01:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './components/demo'
+function App() {
+  return (
+    
+    <div className="App">
+      <h1>HELLO NEC</h1>
+      <Demo></Demo>
+      <Demo />
+      <Demo />
+        </div>
+    
+  );
+}
+
+export default App;
---
+++ demo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const Demo=()=>{
+    return(
+        <h1>THIS IS MY DEMO COMPONENT</h1>
+    );
+}
+export default Demo
